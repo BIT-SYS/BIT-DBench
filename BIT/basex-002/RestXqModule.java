08650a7f31912edd72ee164a6c1724a3e7c39abc
LBJ#0 <41863175+Benjamin-Lau@users.noreply.github.com>
2022-03-11 12:53:24 +0800

    lbj add

---
+++ BIT/basex-002/RestXqModule.java
@@ -0,0 +1,136 @@
+package org.basex.http.restxq;
+
+import static org.basex.query.QueryError.*;
+import static org.basex.util.Token.*;
+
+import java.io.*;
+import java.util.*;
+
+import org.basex.core.*;
+import org.basex.http.*;
+import org.basex.io.*;
+import org.basex.query.*;
+import org.basex.query.func.*;
+
+/**
+ * This class caches information on a single XQuery module with RESTXQ annotations.
+ *
+ * @author BaseX Team 2005-16, BSD License
+ * @author Christian Gruen
+ */
+final class RestXqModule {
+  /** Supported methods. */
+  private final ArrayList<RestXqFunction> functions = new ArrayList<>();
+  /** File reference. */
+  private final IOFile file;
+  /** Parsing timestamp. */
+  private long time;
+
+  /**
+   * Constructor.
+   * @param file xquery file
+   */
+  RestXqModule(final IOFile file) {
+    this.file = file;
+    time = file.timeStamp();
+  }
+
+  /**
+   * Checks the module for RESTXQ annotations.
+   * @param http HTTP context
+   * @return {@code true} if module contains relevant annotations
+   * @throws Exception exception (including unexpected ones)
+   */
+  boolean parse(final HTTPContext http) throws Exception {
+    functions.clear();
+
+    // loop through all functions
+    final Context ctx = http.context(false);
+    try(final QueryContext qc = qc(ctx)) {
+      // loop through all functions
+      final String name = file.name();
+      for(final StaticFunc sf : qc.funcs.funcs()) {
+        // only add functions that are defined in the same module (file)
+        if(name.equals(new IOFile(sf.info.path()).name())) {
+          final RestXqFunction rxf = new RestXqFunction(sf, qc, this);
+          if(rxf.parse(ctx)) functions.add(rxf);
+        }
+      }
+    }
+    return !functions.isEmpty();
+  }
+
+  /**
+   * Checks if the timestamp is still up-to-date.
+   * @return result of check
+   */
+  boolean uptodate() {
+    return time == file.timeStamp();
+  }
+
+  /**
+   * Updates the timestamp.
+   */
+  void touch() {
+    time = file.timeStamp();
+  }
+
+  /**
+   * Returns all functions.
+   * @return functions
+   */
+  ArrayList<RestXqFunction> functions() {
+    return functions;
+  }
+
+  /**
+   * Processes the HTTP request.
+   * @param http HTTP context
+   * @param func function to be processed
+   * @param error optional error reference
+   * @throws Exception exception
+   */
+  void process(final HTTPContext http, final RestXqFunction func, final QueryException error)
+      throws Exception {
+
+    // create new XQuery instance
+    final Context ctx = http.context(false);
+    try(final QueryContext qc = qc(ctx)) {
+      final RestXqFunction rxf = new RestXqFunction(find(qc, func.function), qc, this);
+      rxf.parse(ctx);
+      RestXqResponse.create(rxf, qc, http, error);
+    }
+  }
+
+  // PRIVATE METHODS ====================================================================
+
+  /**
+   * Retrieves a query context for the given module.
+   * @param ctx database context
+   * @return query context
+   * @throws Exception exception
+   */
+  private QueryContext qc(final Context ctx) throws Exception {
+    final QueryContext qc = new QueryContext(ctx);
+    try {
+      qc.parse(string(file.read()), file.path(), null);
+      return qc;
+    } catch(final IOException ex) {
+      // may be triggered when reading the file
+      throw IOERR_X.get(null, ex);
+    }
+  }
+
+  /**
+   * Returns the specified function from the given query context.
+   * @param qctx query context.
+   * @param func function to be found
+   * @return function
+   */
+  private static StaticFunc find(final QueryContext qctx, final StaticFunc func) {
+    for(final StaticFunc sf : qctx.funcs.funcs()) {
+      if(func.info.equals(sf.info)) return sf;
+    }
+    return null;
+  }
+}
---
+++ BIT/basex-007/Nodes.java
@@ -0,0 +1,242 @@
+package org.basex.data;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.basex.core.Context;
+import org.basex.core.Prop;
+import org.basex.io.ArrayOutput;
+import org.basex.util.IntList;
+import org.basex.util.Util;
+
+/**
+ * This class stores node references of a database in an ascending order.
+ * Instances of this class are used in the {@link Context} class to
+ * reference the currently used, marked, and copied nodes.
+ *
+ * @author Workgroup DBIS, University of Konstanz 2005-10, ISC License
+ * @author Christian Gruen
+ */
+public final class Nodes implements Result {
+  /** Full-text position data (for visualization). */
+  public final FTPosData ftpos;
+  /** Root flag (nodes represent all document nodes of the database). */
+  public boolean root;
+  /** Root node. */
+  public Data data;
+  /** Pre values container. */
+  public int[] list;
+  /** Sorted pre values. */
+  public int[] sorted;
+
+  /**
+   * Constructor, specifying a database instance.
+   * @param d data reference
+   */
+  public Nodes(final Data d) {
+    this(new int[0], d);
+  }
+
+  /**
+   * Constructor, specifying a single node and a database instance.
+   * @param n single node
+   * @param d data reference
+   */
+  public Nodes(final int n, final Data d) {
+    this(new int[] { n }, d);
+  }
+
+  /**
+   * Constructor, specifying a node set and a database instance.
+   * @param n node set
+   * @param d data reference
+   */
+  public Nodes(final int[] n, final Data d) {
+    this(n, d, Prop.gui ? new FTPosData() : null);
+  }
+
+  /**
+   * Constructor, specifying a node set, a database instance, and full-text
+   * positions.
+   * @param n node set
+   * @param d data reference
+   * @param ft ft position data
+   */
+  public Nodes(final int[] n, final Data d, final FTPosData ft) {
+    data = d;
+    ftpos = ft;
+    set(n);
+    if(d == null) Util.notexpected("No data available");
+  }
+
+  /**
+   * Constructor, which should only used by test classes.
+   * No database reference is specified.
+   * @param n node set
+   */
+  public Nodes(final int[] n) {
+    list = n;
+    ftpos = null;
+  }
+
+  @Override
+  public long size() {
+    return list.length;
+  }
+
+  @Override
+  public boolean sameAs(final Result v) {
+    final int s = list.length;
+    if(!(v instanceof Nodes) || v.size() != s) return false;
+    final Nodes n = (Nodes) v;
+    if(data != n.data) return false;
+    for(int c = 0; c < s; ++c) if(n.list[c] != list[c]) return false;
+    return ftpos == null || ftpos.sameAs(n.ftpos);
+  }
+
+  /**
+   * Checks if the node set contains all root nodes of the data instance,
+   * and sets the {@link #root} flag.
+   * @return self reference
+   */
+  public Nodes checkRoot() {
+    root = true;
+    int c = 0;
+    for(int p = 0; c < list.length && p < data.meta.size;
+      ++c, p += data.size(p, Data.DOC)) {
+      if(p != list[c]) {
+        root = false;
+        break;
+      }
+    }
+    return this;
+  }
+
+  /**
+   * Checks if the specified node is contained in the array.
+   * @param p pre value
+   * @return true if the node was found
+   */
+  public boolean contains(final int p) {
+    return find(p) >= 0;
+  }
+
+  /**
+   * Returns the position of the specified node or the negative value - 1 of
+   * the position where it should have been found.
+   * @param p pre value
+   * @return true if the node was found
+   */
+  public int find(final int p) {
+    sort();
+    return Arrays.binarySearch(sorted, p);
+  }
+
+  /**
+   * Adds or removes the specified pre node.
+   * @param p pre value
+   */
+  public void toggle(final int p) {
+    final int[] n = new int[] { p };
+    set(contains(p) ? except(list, n) : union(list, n));
+  }
+
+  /**
+   * Merges the specified array with the existing pre nodes.
+   * @param p pre value
+   */
+  public void union(final int[] p) {
+    set(union(list, p));
+  }
+
+  /**
+   * Merges two sorted integer arrays via union.
+   * Note that the input arrays must be sorted.
+   * @param ai first set
+   * @param bi second set
+   * @return resulting set
+   */
+  private static int[] union(final int[] ai, final int[] bi) {
+    final int al = ai.length, bl = bi.length;
+    final IntList c = new IntList();
+    int a = 0, b = 0;
+    while(a != al && b != bl) {
+      final int d = ai[a] - bi[b];
+      c.add(d <= 0 ? ai[a++] : bi[b++]);
+      if(d == 0) ++b;
+    }
+    while(a != al) c.add(ai[a++]);
+    while(b != bl) c.add(bi[b++]);
+    return c.toArray();
+  }
+
+  /**
+   * Subtracts the second from the first array.
+   * Note that the input arrays must be sorted.
+   * @param ai first set
+   * @param bi second set
+   * @return resulting set
+   */
+  private static int[] except(final int[] ai, final int[] bi) {
+    final int al = ai.length, bl = bi.length;
+    final IntList c = new IntList();
+    int a = 0, b = 0;
+    while(a != al && b != bl) {
+      final int d = ai[a] - bi[b];
+      if(d < 0) c.add(ai[a]);
+      else ++b;
+      if(d <= 0) ++a;
+    }
+    while(a != al) c.add(ai[a++]);
+    return c.toArray();
+  }
+
+  /**
+   * Sets the specified nodes.
+   * @param n values
+   */
+  private void set(final int[] n) {
+    list = n;
+    sorted = null;
+  }
+
+  /**
+   * Creates a sorted node array. If the original array is already sorted,
+   * the same reference is used.
+   */
+  private void sort() {
+    if(sorted != null) return;
+    int i = Integer.MIN_VALUE;
+    for(final int n : list) {
+      if(i > n) {
+        sorted = Arrays.copyOf(list, list.length);
+        Arrays.sort(sorted);
+        return;
+      }
+      i = n;
+    }
+    sorted = list;
+  }
+
+  @Override
+  public void serialize(final Serializer ser) throws IOException {
+    for(int c = 0; c < list.length && !ser.finished(); ++c) serialize(ser, c);
+  }
+
+  @Override
+  public void serialize(final Serializer ser, final int n) throws IOException {
+    ser.openResult();
+    ser.node(data, list[n], ftpos);
+    ser.closeResult();
+  }
+
+  @Override
+  public String toString() {
+    final ArrayOutput ao = new ArrayOutput();
+    try {
+      serialize(new XMLSerializer(ao));
+    } catch(final IOException ex) {
+      Util.notexpected(ex);
+    }
+    return ao.toString();
+  }
+}
